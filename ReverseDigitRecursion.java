fe6e633943a20231643000b10ca269278c300c20
chdharm <chaudharydharmpal95@gmail.com>
2017-08-20 17:57:52 +0530

    This is Second commit for this repository

---
+++ ReverseDigitRecursion.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ReverseDigitRecursion {
+
+	
+	
+	public static void main(String [] args){
+		Scanner scan=new Scanner(System.in);
+		
+	}
+	
+	
+	public static int reverse(int num){
+		if(num>0){
+			
+		}
+	}
+}
